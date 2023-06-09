a0c74f332c7fe6d1589fd6a2ca627d7c4595837d
TonNuThaoNhi <103103416+TonNuThaoNhi@users.noreply.github.com>
2023-06-09 22:48:38 +0700

    create reward

--- app/src/main/java/com/example/doan/RewardActivity.java
+++ app/src/main/java/com/example/doan/RewardActivity.java
@@ -3,12 +3,24 @@ package com.example.doan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.doan.databinding.ActivityCategoryBinding;
 
 public class RewardActivity extends AppCompatActivity {
 
+    private ActivityCategoryBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reward);
+        binding= ActivityCategoryBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.backBT.setOnClickListener(new View.OnClickListener() {
+                                              @Override
+                                              public void onClick(View v) {
+                                                  onBackPressed();
+                                              }
+                                          }
+        );
     }
 }
