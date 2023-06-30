a0131516bd07ac7b3c29a40a7888bc444065733f
anhnguyet <anhnguyetbis@gmail.com>
2023-06-30 11:36:15 +0700

    AnhNguyet

--- app/src/main/java/com/example/doan/UploadNewActivity.java
+++ app/src/main/java/com/example/doan/UploadNewActivity.java
@@ -1,6 +1,11 @@
 package com.example.doan;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -8,21 +13,34 @@ import android.view.View;
 import com.example.doan.databinding.ActivityUploadNewBinding;
 public class UploadNewActivity extends AppCompatActivity {
     private ActivityUploadNewBinding binding;
+    private FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding= ActivityUploadNewBinding.inflate(getLayoutInflater());
+        binding = ActivityUploadNewBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         //handle click backSUp( go-back button)
-        binding.backBT1.setOnClickListener(new View.OnClickListener()
-                                          {
-                                              @Override
-                                              public void onClick(View v)
-                                              {
-                                                  onBackPressed();
-                                              }
-                                          }
+        binding.backBT1.setOnClickListener(new View.OnClickListener() {
+                                               @Override
+                                               public void onClick(View v) {
+                                                   onBackPressed();
+                                               }
+                                           }
         );
+    }
+
+    private String tenTV = "", tacgiaTV = "", Mota = "";
+    int sochuongTV;
+
+    private void validateData() {
+
+        //get data
+
+        tenTV = binding.txttenTruyen.getText().toString().trim();
+        tacgiaTV = binding.txttacGia.getText().toString().trim();
+        sochuongTV = Integer.parseInt(binding.txtsoChuong.getText().toString().trim());
+        Mota = binding.txtnoiDung.getText().toString().trim();
     }
 }
