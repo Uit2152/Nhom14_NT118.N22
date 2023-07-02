a4d29c9dca995fd4ad03b5683fed28f1db2faf1a
TonNuThaoNhi <103103416+TonNuThaoNhi@users.noreply.github.com>
2023-07-02 11:54:14 +0700

    ThaoNhi

--- app/src/main/java/com/example/doan/NovelDetailsActivity.java
+++ app/src/main/java/com/example/doan/NovelDetailsActivity.java
@@ -1,18 +1,16 @@
 package com.example.doan;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.example.doan.databinding.ActivityNoveldetailsBinding;
-import com.example.doan.databinding.ActivityReadBinding;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -23,7 +21,7 @@ public class NovelDetailsActivity extends AppCompatActivity {
     private ActivityNoveldetailsBinding binding;
     Context context = NovelDetailsActivity.this;
     private String novelTitle;
-    private String novelID;
+    private int novelID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +29,7 @@ public class NovelDetailsActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         Intent intent = getIntent();
-        novelID= intent.getStringExtra("story_id");
+        novelID= intent.getIntExtra("story_id", 1);
 
         loadNovelDetails();
 
@@ -57,61 +55,62 @@ public class NovelDetailsActivity extends AppCompatActivity {
         });
     }
     private void loadNovelDetails(){
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Truyen");
-        ref.child(novelID).addListenerForSingleValueEvent(new ValueEventListener() {
+        if (novelID == 0) {
+            // handle null novelID
+            return;
+        }
+        DatabaseReference refNovel = FirebaseDatabase.getInstance().getReference("Truyen");
+        refNovel.orderByChild("MaT").equalTo(novelID).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 //get data from firebase
-                novelTitle = "" + snapshot.child("tenTV").getValue();
-                String image = "" + snapshot.child("image").getValue();
-                String description = "" + snapshot.child("Mota").getValue();
-                String timestamp = "" + snapshot.child("timestamp").getValue();
+                if (snapshot.exists()) {
+                    for (DataSnapshot novelSnapshot : snapshot.getChildren()) {
+                        String novelTitle = "" + novelSnapshot.child("tenTV").getValue();
+                        String image = "" + novelSnapshot.child("image").getValue();
+                        String description = "" + novelSnapshot.child("Mota").getValue();
+                        String timestamp = "" + novelSnapshot.child("timestamp").getValue();
+                        String categoryId = "" + novelSnapshot.child("maTL").getValue(); // truy cập đến trường dữ liệu maTL
+                        String author = "" + novelSnapshot.child("tacgiaTV").getValue();
+                        String status = "" + novelSnapshot.child("tinhtrangTV").getValue();
+                        String chapterNumbers = "" + novelSnapshot.child("sochuongTV").getValue();
 
+                        //set data
+                        Glide(false, context, image, binding.imageIV);
+                        binding.tenTV.setText(novelTitle);
+                        binding.summaryTV.setText(description);
+                        binding.tacgiaTV.setText(author);
+                        binding.sochuongTV.setText(chapterNumbers);
+                        binding.tinhtrangTV.setText(status);
 
-                String categoryId = "" + snapshot.child("maTL").getValue();
-                String author = "" + snapshot.child("tacgiaTV").getValue();
-                String status = "" + snapshot.child("tinhtrangTV").getValue();
-                String chapterNumbers = "" + snapshot.child("sochuongTV").getValue();
+                        DatabaseReference refCategory = FirebaseDatabase.getInstance().getReference("Theloai");
+                        refCategory.child(categoryId).addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                String category = "" + snapshot.child("tenTL").getValue();
+                                binding.theloaiTV.setText(category);
+                            }
 
-                //format date
-                String date = MyApplication.formatTimestamp(Long.parseLong(timestamp));
-
-
-                //set data
-                Glide(false, context, image, binding.imageIV);
-
-                binding.tenTV.setText(novelTitle);
-
-                binding.summaryTV.setText(description);
-
-                binding.tacgiaTV.setText(author);
-                binding.sochuongTV.setText(chapterNumbers);
-                binding.tinhtrangTV.setText(status);
-
-                DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Theloai");
-                ref.child(categoryId).addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-
-                        String category = "" + snapshot.child("tenTL").getValue();
-                        binding.theloaiTV.setText(category);
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+                                // handle error
+                            }
+                        });
                     }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-
-                    }
-                });
+                } else {
+                    // handle not found
+                }
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
+                // handle error
             }
         });
     }
     public void Glide(Boolean isUser, Context context, String Url, ImageView Image) {
         try {
-                Glide.with(context).load(Url).placeholder(R.color.image_profile).into(Image);
+            Glide.with(context).load(Url).placeholder(R.color.image_profile).into(Image);
 
         } catch (Exception e) {
             Image.setImageResource(R.drawable.basic_book);
